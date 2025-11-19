6eb2abcb267cc0dfe42cc96a804e05a18f4278e9
Brian Cooper <brian@brian-cooper.com>
2025-11-19 13:48:07 -0800

    chore: sync with upstream

--- examples/vite.config.mjs
+++ examples/vite.config.mjs
@@ -1,17 +1,31 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import mkcert from "vite-plugin-mkcert";
 
-const entries = { main: 'index.html' };
-['01-helloworld', '02-gps-and-sensors', '03-api-communication', '04-aframe', '05-aframe-js', '06-aframe-api-comm', 'devorient'].forEach ( example => {
-    entries[example] = resolve(__dirname, `${example}/index.html`);
+const entries = { main: "index.html" };
+[
+  "01-helloworld",
+  "02-gps-and-sensors",
+  "03-api-communication",
+  "04-aframe",
+  "05-aframe-js",
+  "06-aframe-api-comm",
+  "devorient",
+].forEach((example) => {
+  entries[example] = resolve(__dirname, `${example}/index.html`);
 });
 
 export default defineConfig({
-    base: '/locar.js',
-    build: {
-        outDir: '../docs',
-        rollupOptions: {
-            input: entries 
-        }
-    }
+  base: "/locar.js",
+  plugins: [mkcert()],
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
+  build: {
+    outDir: "../docs",
+    rollupOptions: {
+      input: entries,
+    },
+  },
 });
